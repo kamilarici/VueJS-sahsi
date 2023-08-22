1f90cd493710e3b4234426089cf651d8bc849c73
KamilArici <kamilarici5@gmail.com>
2023-08-22 13:12:10 +0300

    data return

--- 02-vue-uygulama/app.js
+++ 02-vue-uygulama/app.js
@@ -1,6 +1,19 @@
 const app = Vue.createApp({
   //? html içine yazılan h2 taglerini eziyor
-  template: "<h2>bu ilk uygulamamiz</h2>",
+  //   template: "<h2>bu ilk uygulamamiz</h2>",
+  template: `
+ <h2>başlik:{{baslik}}</h2>
+<h3>içerik:{{icerik}} </h3>
+<p>yorum sayisi: {{yorumSayisi}}</p>
+
+ `,
+  data() {
+    return {
+      baslik: "Vue Dersi",
+      icerik: "vue 3 konulari",
+      yorumSayisi: 7,
+    };
+  },
 });
 
 app.mount("#app");
