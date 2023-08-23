2a910218c5cdb73a2578e024fcd2b5b035fef3fb
KamilArici <kamilarici5@gmail.com>
2023-08-23 21:24:59 +0300

    iki yönlü data binding

---
+++ 06-data-binding/src/components/Form.vue
@@ -0,0 +1,20 @@
+<template>
+  <form>
+    <label for="">email</label>
+    <br />
+    <input type="email" v-model="email" />
+    {{ email }}
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: "sdfs",
+    };
+  },
+};
+</script>
+
+<style></style>
