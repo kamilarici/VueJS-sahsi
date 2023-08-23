c356f924eb9db692aedc1ce78ed53ecbf909fe0e
KamilArici <kamilarici5@gmail.com>
2023-08-23 19:51:28 +0300

    edit

---
+++ 05-cli/kkkk/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <Deneme />
+  <Header :baslik="yazi" />
+  <h1>CLI ile proje</h1>
+</template>
+
+<script>
+import Header from "./components/Header";
+import Deneme from "./components/Deneme";
+export default {
+  name: "App",
+  components: {
+    Header,
+    Deneme,
+  },
+  data() {
+    return {
+      yazi: "merhaba vue 33",
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 05-cli/kkkk/src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>
+      {{ baslik }}
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["baslik"],
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+/* h1 {
+  color: red;
+  font-size: 40px;
+} */
+</style>
