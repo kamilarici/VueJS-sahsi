3b48bd9e588c8a75ac85c8728278549725447272
KamilArici <kamilarici5@gmail.com>
2023-08-23 23:01:13 +0300

    hobiler

--- 06-data-binding/src/components/Form.vue
+++ 06-data-binding/src/components/Form.vue
@@ -1,18 +1,73 @@
 <template>
-  <form>
-    <label for="">email</label>
-    <br />
-    <input type="email" v-model="email" />
-    {{ email }}
-  </form>
+  <div>
+    <form>
+      <label for="">email</label>
+      <br />
+      <input type="email" v-model="email" />
+      <!-- {{ email }} -->
+      <br />
+      <label for="">yetki</label>
+      <br />
+      <select v-model="yetki">
+        <option>editor</option>
+        <option>admin</option>
+      </select>
+      <!-- {{ yetki }} -->
+      <br />
+      <br />
+      <input type="checkbox" v-model="anlasma" />
+
+      <label for="">kabul ediyorum</label>
+      <hr />
+      <p>anlaşma kabulu: {{ anlasma ? "ediyorum" : "etmiyorum" }}</p>
+      <hr />
+      <div>
+        <input type="checkbox" value="yönetim" v-model="istekler" />
+        <label>yönetim</label>
+      </div>
+      <div>
+        <input type="checkbox" value="erişim" v-model="istekler" />
+        <label>erişim</label>
+      </div>
+      <div>
+        <input type="checkbox" value="silme" v-model="istekler" />
+        <label>silme</label>
+      </div>
+      <p>istekler:{{ istekler }}</p>
+    </form>
+    //? *********
+    <form>
+      <label>hobiler</label>
+      <input type="text" v-model="aktifHobi" @keyup="hobiEkle" />
+      <div v-for="hobi in hobiler" :key="hobi">
+        {{ hobi }}
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      email: "sdfs",
+      email: "",
+      yetki: "editor",
+      anlasma: false,
+      istekler: [],
+      hobiler: [],
+      aktifHobi: "",
     };
+  },
+  methods: {
+    hobiEkle($event) {
+      console.log($event);
+      if ($event.key == "," && this.aktifHobi) {
+        if (!this.hobiler.includes(this.aktifHobi)) {
+          this.hobiler.push(this.aktifHobi);
+        }
+        this.aktifHobi = "";
+      }
+    },
   },
 };
 </script>
