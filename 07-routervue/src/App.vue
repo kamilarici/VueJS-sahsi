67d5d1fdf00a1206c1863a2e6ae6a05d271a3aaa
KamilArici <kamilarici5@gmail.com>
2023-08-24 23:58:11 +0300

    router

--- 07-routervue/src/App.vue
+++ 07-routervue/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <!-- <router-link to="/personel"> personel</router-link> -->
+    <router-link to="{name:'personel'}"> personel</router-link>
   </nav>
   <router-view />
 </template>
