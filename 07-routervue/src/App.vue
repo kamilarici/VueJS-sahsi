df64989e1edd2585b59f73fb21ddd96ddc0d3cac
KamilArici <kamilarici5@gmail.com>
2023-08-24 23:13:35 +0300

    router deneme

--- 07-routervue/src/App.vue
+++ 07-routervue/src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
+    <router-link :to="{ name: 'home' }">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
