bf922b4a43bb85bf5d25dedbaf0bed82dcac08a2
KamilArici <kamilarici5@gmail.com>
2023-08-29 11:31:42 +0300

    data 2

--- vue3-tekrar/03-uygulama oluşturma/app.js
+++ vue3-tekrar/03-uygulama oluşturma/app.js
@@ -1,12 +1,14 @@
 const app = Vue.createApp({
   template: `
   <h2>Başlik: {{baslik}}</h2>
-  <h3>İçerik</h3>
-  <p>uygulama sayisi</p> 
+  <h3>İçerik: {{icerik}} </h3>
+  <p>uygulama sayisi: {{appAdet}} </p> 
   `,
   data() {
     return {
       baslik: "vue 3 ile web --- sayfalari",
+      icerik: "composition api-router-firebase",
+      appAdet: 5,
     };
   },
 });
