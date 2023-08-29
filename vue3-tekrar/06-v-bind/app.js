e8fd0ed8b25e27ff30708366f0726dcc05ed03d4
KamilArici <kamilarici5@gmail.com>
2023-08-29 12:59:10 +0300

    v-bind:style="style" yada :style="style"

--- vue3-tekrar/06-v-bind/app.js
+++ vue3-tekrar/06-v-bind/app.js
@@ -1,9 +1,14 @@
 const app = Vue.createApp({
+  //? style html etribute olduğu için direk kullanamayız ve v-bind ile kullanmamız gerekiyor (data aracılığı ile)
+  //? v-bind:style="style" yerine :style="style" şeklinde de kullanabiliriz
   template: `
-
+  <div v-bind:style="style">kirmizi</div>
+  <div :style="style">kirmizi</div>
   `,
   data() {
-    return {};
+    return {
+      style: "color:red",
+    };
   },
   methods: {},
 });
