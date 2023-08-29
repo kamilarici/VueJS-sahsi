53cac32c75f008993a2f3d97016be5f86dfee2e2
KamilArici <kamilarici5@gmail.com>
2023-08-29 11:50:26 +0300

    @click

--- vue3-tekrar/03-uygulama oluşturma/app.js
+++ vue3-tekrar/03-uygulama oluşturma/app.js
@@ -4,12 +4,14 @@ const app = Vue.createApp({
   <h3>İçerik: {{icerik}} </h3>
   <p>uygulama sayisi: {{appAdet}} </p> 
   <button v-on:click="appEkle()">uygulama ekle</button>
+  <p @click="appEkle">{{metin}}</p>
   `,
   data() {
     return {
       baslik: "vue 3 ile web --- sayfalari",
       icerik: "composition api-router-firebase",
       appAdet: 5,
+      metin: "metin eklemek için tiklayiniz",
     };
   },
   methods: {
@@ -18,6 +20,7 @@ const app = Vue.createApp({
       console.log("appEkle metodu çalıştı");
       //? butona tıklandığında appAdet artırmak için appEkle fonsiyonu içinde this ile appAdet e ulaşıp değerini 1 artırabiliriz
       this.appAdet++;
+      this.metin = `${this.appAdet} tane uygulama var`;
     },
   },
 });
