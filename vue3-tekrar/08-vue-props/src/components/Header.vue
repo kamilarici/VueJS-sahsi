0ee4c84e034d0034fd12dc1cd0efa0ec745260e3
KamilArici <kamil@testinvite.com>
2023-08-30 18:41:25 +0300

    props string

---
+++ vue3-tekrar/08-vue-props/src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1> {{ baslik }} </h1>
+</template>
+
+<script>
+export default {
+    props:["baslik"],
+data(){
+    return{
+        
+    }
+
+}
+}
+</script>
+
+<style>
+
+</style>
