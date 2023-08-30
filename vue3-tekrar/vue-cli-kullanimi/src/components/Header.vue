9dfaaa3cac68fb32a558759252812f92af74c8bf
KamilArici <kamil@testinvite.com>
2023-08-30 18:07:41 +0300

    global style

--- vue3-tekrar/vue-cli-kullanimi/src/components/Header.vue
+++ vue3-tekrar/vue-cli-kullanimi/src/components/Header.vue
@@ -19,8 +19,8 @@ export default {
 
 <!-- //?  sadece bu componente geçerli olsun istiyorsak scoped deriz ama global olarak tüm h1 ler aynı style alsın istersek scoped vermeyiz  -->
 <style scoped >
-h1{
+/* h1{
   color: red;
   font-size: 40px;
-}
+} */
 </style>
