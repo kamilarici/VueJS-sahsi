03ffc1b1e6cf94891bdb09b154569c092b5fb4bf
KamilArici <kamil@testinvite.com>
2023-08-30 10:06:45 +0300

    filter-secilenicecekler

---
+++ vue3-tekrar/07-mini uygulama/app.js
@@ -0,0 +1,32 @@
+const app = Vue.createApp({
+  template: `
+  <h2>icecek seciniz</h2>
+  <ul>
+  <li v-for="i in icecekler" class="li" :class="{secilen:i.sec}" @click="icecekSec(i)"> {{i.isim}} </li>
+</ul>
+<button @click="secilenIceceklerGetir">secilen icecekler</button>
+  `,
+  data() {
+    return {
+icecekler:[
+  {isim:"su", sec:true},
+  {isim:"ayran", sec:false},
+  {isim:"kola", sec:false},
+  {isim:"salgam", sec:false},
+]
+       
+    };
+  },
+  methods: {
+    icecekSec(icecek){
+      icecek.sec=!icecek.sec;
+    },
+      secilenIceceklerGetir(){
+        const secilenler=this.icecekler.filter(i=>i.sec==true);
+        console.log(secilenler);
+      }
+    
+  },
+});
+
+app.mount("#uygulama");
