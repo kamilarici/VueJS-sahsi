cb1172b6a394792719877902028e4a95fc1e8abb
KamilArici <kamil@testinvite.com>
2023-08-31 15:27:40 +0300

    to do start

---
+++ vue3-tekrar/13-vue-todo-app/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  
+ 
+  <router-view/>
+</template>
+
+<style>
+body{
+  background-color: #2f3542;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #f1f2f6;
+  max-width: 800px;
+  margin: 0 auto;
+}
+
+
+</style>
---
+++ vue3-tekrar/13-vue-todo-app/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    Todo App
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'HomeView',
+  components: {
+   
+  }
+}
+</script>
