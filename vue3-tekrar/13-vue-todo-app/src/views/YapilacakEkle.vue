abeab262b6907d06955306d593acf0045a1c1049
KamilArici <kamil@testinvite.com>
2023-09-01 11:30:11 +0300

    yapilacak ekle

---
+++ vue3-tekrar/13-vue-todo-app/src/views/YapilacakEkle.vue
@@ -0,0 +1,24 @@
+<template>
+  <form @submit.prevent="yapilacakEkle">
+<label >Baslik:</label>
+<input type="text" v-model="baslik" required>
+<label >İçerik:</label>
+<input type="text" v-model="icerik" required>
+
+  </form>
+</template>
+
+<script>
+export default {
+data(){
+  return{
+    baslik:"",
+    icerik:""
+  }
+}
+}
+</script>
+
+<style>
+
+</style>
