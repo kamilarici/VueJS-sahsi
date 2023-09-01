ac41d908e86fe4e9f07ec9f29528b1aff9181601
KamilArici <kamil@testinvite.com>
2023-09-01 11:47:58 +0300

    yapilacak ekle clg

--- vue3-tekrar/13-vue-todo-app/src/router/index.js
+++ vue3-tekrar/13-vue-todo-app/src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import YapilacakEkle from '../views/YapilacakEkle.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/yapilacak-ekle',
+    name: 'YapilacakEkle',
+    component: YapilacakEkle
   },
   
 ]
