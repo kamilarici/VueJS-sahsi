c4b2ea4141edb840915ae05966ed9354fdcca2b0
KamilArici <kamil@testinvite.com>
2023-09-02 18:53:55 +0300

    computed input

---
+++ vue3-tekrar/14-composition-app/set-up/src/views/SetupComp.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>set up comp</h1>
+    <input type="text" v-model="ara">
+<div v-for="isim in bulunanlar">
+{{ isim }}
+</div>
+  </div>
+</template>
+
+<script>
+import { ref,computed,watch,watchEffect } from 'vue';
+export default {
+setup(){
+  const isimler=ref(['luffy', 'zoro','sanji','nami','usop'])  
+  const ara=ref('')
+
+  const bulunanlar=computed(()=>{
+    return isimler.value.filter((isim)=>isim.includes(ara.value))
+  })
+  watch(ara)
+  return {isimler,ara,bulunanlar}
+}
+}
+</script>
+
+<style>
+
+</style>
