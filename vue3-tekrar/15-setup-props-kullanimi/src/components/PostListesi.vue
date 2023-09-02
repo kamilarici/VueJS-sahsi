4752615a194931f7290517f788f4bde9274c0f2e
KamilArici <kamil@testinvite.com>
2023-09-02 21:17:03 +0300

    edit2

---
+++ vue3-tekrar/15-setup-props-kullanimi/src/components/PostListesi.vue
@@ -0,0 +1,21 @@
+<template>
+  <div v-for="post in postlar" :key="post.id">
+  {{ post.baslik }}
+  </div>
+</template>
+
+<script>
+export default {
+props:['postlar'],
+//? setup fonk olmadan da fonk kullanabiliriz yalnız setup içinde kullanmak için props olarak tanımlamamız gerekir 
+// setup(props){
+//     console.log(props.postlar);
+// }
+
+
+}
+</script>
+
+<style>
+
+</style>
