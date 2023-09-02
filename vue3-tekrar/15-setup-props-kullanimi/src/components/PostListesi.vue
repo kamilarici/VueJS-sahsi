9f2b4e440c2accdbd9ea47cf6fffce8ff7c54121
KamilArici <kamil@testinvite.com>
2023-09-02 21:31:33 +0300

    setup fetch kullanımı

--- vue3-tekrar/15-setup-props-kullanimi/src/components/PostListesi.vue
+++ vue3-tekrar/15-setup-props-kullanimi/src/components/PostListesi.vue
@@ -1,17 +1,36 @@
 <template>
   <div v-for="post in postlar" :key="post.id">
-  {{ post.baslik }}
+  {{ post.title }}
   </div>
 </template>
 
 <script>
+import {ref} from 'vue'
 export default {
-props:['postlar'],
+  //
+// props:['postlar'],
+//
 //? setup fonk olmadan da fonk kullanabiliriz yalnız setup içinde kullanmak için props olarak tanımlamamız gerekir 
 // setup(props){
 //     console.log(props.postlar);
 // }
 
+setup(){
+  const postlar=ref([])
+  const hatalar=ref(null)
+
+
+  const verileriYukle=async()=>{
+    let veriler=await fetch('https://jsonplaceholder.typicode.com/posts');
+    postlar.value=await veriler.json()
+  }
+
+verileriYukle()
+
+return {postlar,hatalar}
+
+
+}
 
 }
 </script>
