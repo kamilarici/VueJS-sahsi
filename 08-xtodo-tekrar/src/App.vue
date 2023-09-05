93a942e3f23d52857d93c65533b8214413ba35bf
KamilArici <kamil@testinvite.com>
2023-09-05 22:33:13 +0300

    edit

--- 08-xtodo-tekrar/src/App.vue
+++ 08-xtodo-tekrar/src/App.vue
@@ -1,11 +1,46 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <!-- <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> -->
+    <div class="contetnt mx-5"></div>
+    <h1>Todo App</h1>
+    <form @submit.prevent="addTodo">
+      <div class="field">
+        <div class="control">
+          <input
+            type="text"
+            v-model="todo"
+            class="input"
+            placeholder="Todo Girin"
+          />
+        </div>
+      </div>
+      <button type="submit" class="button is-warning">Ekle</button>
+    </form>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    const todo = ref("");
+    const todos = ref([]);
+
+    function addTodo() {
+      todos.value.push({
+        done: false,
+        content: todo.value,
+        id: Date.now(),
+      });
+      todo.value = "";
+    }
+
+    return { todo, todos };
+  },
+};
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
