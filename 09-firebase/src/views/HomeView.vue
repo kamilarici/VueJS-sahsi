c7082a4e9911be93bfee7f9aa842efab7181a651
KamilArici <kamil@testinvite.com>
2023-09-10 22:48:17 +0300

    clg

--- 09-firebase/src/views/HomeView.vue
+++ 09-firebase/src/views/HomeView.vue
@@ -3,8 +3,28 @@
 </template>
 
 <script>
+import { onMounted } from "vue";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  get,
+} from "firebase/firestore/lite";
+
+import { fb } from "../firebase/config";
 export default {
   name: "HomeView",
   components: {},
+  setup() {
+    onMounted(async () => {
+      const db = getFirestore(fb);
+      const fbDataName = collection(db, "books");
+      const fbDocs = await getDocs(fbDataName);
+      const data = fbDocs.docs.map((doc) => doc.data());
+
+      console.log(data);
+      return data;
+    });
+  },
 };
 </script>
