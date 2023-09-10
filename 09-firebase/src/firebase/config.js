cb75c9399a86b896d22c2f3dad8ec4c684cd95be
KamilArici <kamil@testinvite.com>
2023-09-10 22:28:14 +0300

    config

---
+++ 09-firebase/src/firebase/config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBRA8K7htbgxYagKqq2zD2eaVsAjcG9rxU",
+  authDomain: "vue3-btk-f0d2b.firebaseapp.com",
+  projectId: "vue3-btk-f0d2b",
+  storageBucket: "vue3-btk-f0d2b.appspot.com",
+  messagingSenderId: "533400473884",
+  appId: "1:533400473884:web:d3a4de307f79604c8c2ad9",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
