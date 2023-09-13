bc88bf70c242726e71b70764ab0d9d1ec32f9b44
KamilArici <kamil@testinvite.com>
2023-09-13 21:29:47 +0300

    event

---
+++ 05-cli/vue3-cli-component/src/components/Button.vue
@@ -0,0 +1,21 @@
+<template>
+  <div @click="clean">
+    <h3>temizle</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  //    emit yukarı veri yollamak için kullanılır
+  methods: {
+    clean() {
+      this.$emit;
+    },
+  },
+};
+</script>
+
+<style></style>
