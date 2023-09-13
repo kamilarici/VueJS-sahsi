b3f386e8cd61e63cb86a2b810a4b7cb4370cf708
KamilArici <kamil@testinvite.com>
2023-09-13 23:05:01 +0300

    select binding

--- 06-02/src/components/Form.vue
+++ 06-02/src/components/Form.vue
@@ -2,6 +2,14 @@
   <form>
     <input type="text" v-model="text" />
     <p>{{ text }}</p>
+
+    <select v-model="framework">
+      <!-- ?value hiç girmez isek textleri value kabul eder ve alttaki p tag içine yazar ama value değerleri girersek seçtiğimiz değerleri değil seştiğimiz textlerin valularını yazar p tag içine  -->
+      <option value="1">vue</option>
+      <option value="2">react</option>
+      <option value="3">svelte</option>
+    </select>
+    <p>{{ framework }}</p>
   </form>
 </template>
 
@@ -10,6 +18,7 @@ export default {
   data() {
     return {
       text: "deneme",
+      framework: "",
     };
   },
 };
