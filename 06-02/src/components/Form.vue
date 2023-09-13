e8362f39f0420e29bf00fb949ac57be109063bb8
KamilArici <kamil@testinvite.com>
2023-09-13 22:58:43 +0300

    select binding

---
+++ 06-02/src/components/Form.vue
@@ -0,0 +1,18 @@
+<template>
+  <form>
+    <input type="text" v-model="text" />
+    <p>{{ text }}</p>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: "deneme",
+    };
+  },
+};
+</script>
+
+<style></style>
