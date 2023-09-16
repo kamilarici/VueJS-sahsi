6a5f8dfb70cf3b0ec1a7a1e8de6b3c26558d3e80
KamilArici <kamil@testinvite.com>
2023-09-16 23:10:23 +0300

    props :id

--- 09-vue-router/src/views/StudentInfo.vue
+++ 09-vue-router/src/views/StudentInfo.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
     <h3>Ögrenci Bilgi Sayfasi</h3>
-    <p>Ögrenci ID: {{ studentID }}</p>
+    <!-- <p>Ögrenci ID: {{ studentID }}</p> -->
+    <p>Ögrenci ID: {{ id }}</p>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      studentID: this.$route.params.id,
-    };
-  },
+  // data() {
+  //   return {
+  //     studentID: this.$route.params.id,
+  //   };
+  // },
+  // ? props true olduğu için burada data tanımlamamıza gerek yok
+  props: ["id"],
 };
 </script>
 
