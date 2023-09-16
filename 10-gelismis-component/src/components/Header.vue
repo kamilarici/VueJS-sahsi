b0f4a2af0ee638ddd9547999426e6bd19ea4e576
KamilArici <kamil@testinvite.com>
2023-09-17 00:11:04 +0300

    computed

--- 10-gelismis-component/src/components/Header.vue
+++ 10-gelismis-component/src/components/Header.vue
@@ -4,12 +4,28 @@
 
     <!-- ?App.vue comp içerisinde  header içine h1 tag yazdık ve burada gelmesi istediğimiz yere slot koymamız yeterlidir  -->
     <slot></slot>
+    <hr />
+    <input type="text" v-model="name" />
+    <input type="text" v-model="surname" />
+    <p>{{ nameSurname }}</p>
   </div>
 </template>
 
 <script>
 export default {
   //   props: ["content"],
+  data() {
+    return {
+      name: "",
+      surname: "",
+    };
+  },
+  computed: {
+    //* bu işlemi method ile de yapabiliriz ama çalışma mantığı olarak bu daha performanslı çalışır
+    nameSurname() {
+      return this.name + " " + this.surname;
+    },
+  },
 };
 </script>
 
