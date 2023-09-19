4af133a107e945b90ca4032699fc702f6db2834f
KamilArici <kamil@testinvite.com>
2023-09-19 23:55:52 +0300

    added

--- 11-planner-uygulama-2/src/views/Home.vue
+++ 11-planner-uygulama-2/src/views/Home.vue
@@ -19,7 +19,7 @@ export default {
     };
   },
   mounted() {
-    fetch("http://localhost:3000/project")
+    fetch("http://localhost:3000/projects")
       .then((res) => res.json())
       .then((data) => (this.projects = data))
       .catch((err) => console.log(err.message));
