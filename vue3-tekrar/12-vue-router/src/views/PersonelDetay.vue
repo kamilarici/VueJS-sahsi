460ac29bed665a8e33002ebb68c50c4fdd88c653
KamilArici <kamil@testinvite.com>
2023-09-20 23:06:26 +0300

    edit

--- vue3-tekrar/12-vue-router/src/views/PersonelDetay.vue
+++ vue3-tekrar/12-vue-router/src/views/PersonelDetay.vue
@@ -1,22 +1,20 @@
 <template>
- <h1>personel detay sayfasi</h1>
- <!-- <p>personel id {{ $route.params.id }} </p> -->
- <!-- <p>personel id {{ route }} </p> -->
- <p>personel id {{ id}} </p>
+  <h1>personel detay sayfasi</h1>
+  <!-- <p>personel id {{ $route.params.id }} </p> -->
+  <!-- <p>personel id {{ route }} </p> -->
+  <p>personel id {{ id }}</p>
 </template>
 
 <script>
 export default {
-    //? direk props ile id yi yakalayabiliriz(öncesi index.js e props :true yapmak gerekiyor)
-    props:['id'],
-// data(){
-//     return{
-//         route:this.$route.params.id
-//     }
-// }
-}
+  //? direk props ile id yi yakalayabiliriz(öncesi index.js e props :true yapmak gerekiyor.)
+  props: ["id"],
+  // data(){
+  //     return{
+  //         route:this.$route.params.id
+  //     }
+  // }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
