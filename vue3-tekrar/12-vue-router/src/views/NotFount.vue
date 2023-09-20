11e307485098b515d278a612d41bbd8995b8b41b
KamilArici <kamil@testinvite.com>
2023-09-20 23:07:01 +0300

    edit

--- vue3-tekrar/12-vue-router/src/views/NotFount.vue
+++ vue3-tekrar/12-vue-router/src/views/NotFount.vue
@@ -1,19 +1,17 @@
 <template>
-  <h1>SAYFA BULUNAMADI</h1>
+  <h1>****SAYFA BULUNAMADI****</h1>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style scoped>
-h1{
-    margin-top: 30vh;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    color: red;
+h1 {
+  margin-top: 30vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: rgb(53, 196, 65);
 }
 </style>
