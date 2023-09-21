5dd5a9a075c70459973073a81c3895ef97cd22ba
KamilArici <kamil@testinvite.com>
2023-09-21 22:34:53 +0300

    ..

--- vue3-tekrar/09-vue-event-aktarimi/src/App.vue
+++ vue3-tekrar/09-vue-event-aktarimi/src/App.vue
@@ -1,26 +1,24 @@
 <template>
   <!-- //? dinlediğimiz yerdeki metodu event olarak kullanıp temizle div ine basıldığında input içi temizlenir -->
- <Header @temizle="sil"/>
- <input ref="metin" />
- <!-- //? ref (referans) Vue.js'de kullanılan bir özelliktir ve DOM öğelerine veya Vue bileşenlerine erişmek için kullanılır. ref ile bir DOM öğesine veya bileşene bir referans vererek, JavaScript kodunuzun bu öğeye erişmesini ve üzerinde işlem yapmasını sağlayabilirsiniz. -->
+  <Header @temizle="sil" />
+  <input ref="metin" />
+  <!-- //? ref (referans) Vue.js'de kullanılan bir özelliktir ve DOM öğelerine veya Vue bileşenlerine erişmek için kullanılır. ref ile bir DOM öğesine veya bileşene bir referans vererek, JavaScript kodunuzun bu öğeye erişmesini ve üzerinde işlem yapmasını sağlayabilirsiniz. -->
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
-import Header from './components/Header.vue';
+import Header from "./components/Header.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-  Header,
-    
+    Header,
   },
-  methods:{
-    sil(){
-      this.$refs.metin.value=""
-    }
-  }
-
-}
+  methods: {
+    sil() {
+      this.$refs.metin.value = "";
+    },
+  },
+};
 </script>
 
 <style>
