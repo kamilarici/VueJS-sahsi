465356ba33363f068bfff171e3c21d2694b2d3c2
KamilArici <kamil@testinvite.com>
2023-09-23 01:15:08 +0300

    add project

--- 11-planner-uygulama-2/src/views/AddProject.vue
+++ 11-planner-uygulama-2/src/views/AddProject.vue
@@ -17,7 +17,18 @@ export default {
   },
   methods: {
     handleSubmit() {
-      console.log(this.title, this.details);
+      let project = {
+        title: this.title,
+        details: this.details,
+        complete: false,
+      };
+      fetch("http://localhost:3000/projects", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(project),
+      })
+        .then(() => this.$router.push("/"))
+        .catch((err) => console.log(err));
     },
   },
 };
