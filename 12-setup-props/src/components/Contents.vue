f8907bf33daf0e0a3eb81a567354c8d4ab8cff52
KamilArici <kamil@testinvite.com>
2023-09-23 21:48:24 +0300

    composables and

--- 12-setup-props/src/components/Contents.vue
+++ 12-setup-props/src/components/Contents.vue
@@ -11,23 +11,26 @@
 
 <script>
 import { ref } from "vue";
+import getContentsFetch from "../composables/getContentsFetch";
 export default {
   //   props: ["contents"],
   //   setup içinde props kullanmak için setup(props) şeklinde tanımlamak gerekiyor
   setup() {
-    const contents = ref([]);
-    const err = ref(null);
-    const getContents = async () => {
-      try {
-        let data = await fetch("https://jsonplaceholder.typicode.com/posts");
-        contents.value = await data.json();
-        if (!data.ok) {
-          throw new Error("verilere erişilemedi");
-        }
-      } catch (error) {
-        err.value = error.message;
-      }
-    };
+    // const contents = ref([]);
+    // const err = ref(null);
+    // const getContents = async () => {
+    //   try {
+    //     let data = await fetch("https://jsonplaceholder.typicode.com/posts");
+    //     contents.value = await data.json();
+    //     if (!data.ok) {
+    //       throw new Error("verilere erişilemedi");
+    //     }
+    //   } catch (error) {
+    //     err.value = error.message;
+    //   }
+    // };
+
+    const { contents, err, getContents } = getContentsFetch();
     getContents();
 
     return { contents, err };
