c11e7f6367f9d35fad9d8befe4857aed0da1c987
KamilArici <kamil@testinvite.com>
2023-09-23 19:21:36 +0300

    computed

--- 12-setup-computed/src/views/HomeView.vue
+++ 12-setup-computed/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="home">
-    <div v-for="day in days" :key="day">
+    <input type="text" v-model="search" />
+    <div v-for="day in finded" :key="day">
       {{ day }}
     </div>
   </div>
+  <!-- ? computed  ile arama ve filtreleme işlemi yaptık..inputa v- model ile sarch değeri verdik ve başlangıç ref değerini "" verdik  sonrası for ile dönerken filtrelediğimiz finded leri dönüyoruz computed ile de input içine girilen değerler sayesinde   filtrelenmiş değerler önüöüze düşüyor -->
 </template>
 
 <script>
@@ -13,8 +15,11 @@ export default {
   components: {},
   setup() {
     const days = ref(["pazartesi", "salı", "çarşamba", "perşembe", "cuma"]);
-
-    return { days };
+    const search = ref("");
+    const finded = computed(() => {
+      return days.value.filter((day) => day.includes(search.value));
+    });
+    return { days, finded, search };
   },
 };
 </script>
