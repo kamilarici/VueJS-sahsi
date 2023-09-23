f2cb203a66e975995eae9269c459c95504a5d482
KamilArici <kamil@testinvite.com>
2023-09-23 17:28:24 +0300

    composition api start

---
+++ 12-composition-api/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">home</div>
+</template>
+
+<script>
+import { onMounted } from "vue";
+export default {
+  name: "HomeView",
+  components: {},
+  setup() {
+    console.log("setup yüklendi");
+    onMounted(() => {
+      console.log("mounted yüklendi");
+    });
+  },
+};
+</script>
