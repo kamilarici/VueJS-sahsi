261d5443fd403e8ab71f2f12e3f738376c6c5eb3
KamilArici <kamil@testinvite.com>
2023-09-23 18:54:44 +0300

    computed

---
+++ 12-setup-computed/src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <div v-for="day in days" :key="day">
+      {{ day }}
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref, computed } from "vue";
+export default {
+  name: "HomeView",
+  components: {},
+  setup() {
+    const days = ref(["pazartesi", "salı", "çarşamba", "perşembe", "cuma"]);
+
+    return { days };
+  },
+};
+</script>
