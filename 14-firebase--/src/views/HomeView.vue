ca7e52e791a685fb20d0789df994c789a1ccc764
KamilArici <kamil@testinvite.com>
2023-09-24 13:00:35 +0300

    firebase end

--- 14-firebase--/src/views/HomeView.vue
+++ 14-firebase--/src/views/HomeView.vue
@@ -13,8 +13,12 @@ import {
   get,
   addDoc,
   deleteDoc,
+  updateDoc,
   doc,
-} from "firebase/firestore/lite";
+  query,
+  where,
+  onSnapshot,
+} from "firebase/firestore";
 import { fb } from "../firebase/config";
 export default {
   name: "HomeView",
@@ -32,8 +36,24 @@ export default {
       //   author: "Platon",
       //   pageCount: 200,
       // });
-      const docRef = doc(db, "books", "oOCnhRWDqpN1PuPeZMbd");
-      deleteDoc(docRef);
+      //**************** */
+      // const docRef = doc(db, "books", "aJtnDQRTa2tZEEnzDefo");
+      // deleteDoc(docRef);
+      //*********************** */
+      //*istediğim verileri sağlayan verileri çağırmış olduk
+      const q = query(fbRef, where("pageCount", "==", 500));
+      onSnapshot(q, (ss) => {
+        let books = [];
+        ss.forEach((doc) => {
+          books.push({ ...doc.data(), id: doc.id });
+        });
+        console.log(books);
+      });
+
+      const docRef = doc(db, "books", "3tMdxvaitdteB6EdeQqX");
+      updateDoc(docRef, {
+        pageCount: 399,
+      });
       return data;
     });
   },
