a94efc7642810231b102c944709b2ba9bf848d50
KamilArici <kamil@testinvite.com>
2023-09-24 10:42:22 +0300

    firebase start

---
+++ 14-firebase--/src/firebase/config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDp1sz-vLlAADAyLWm-Gtp9UuyUoP_703M",
+  authDomain: "vue3-btk-fb9cf.firebaseapp.com",
+  projectId: "vue3-btk-fb9cf",
+  storageBucket: "vue3-btk-fb9cf.appspot.com",
+  messagingSenderId: "41163470434",
+  appId: "1:41163470434:web:2b2f76ede1c68d67c0833b",
+};
+
+const fb = initializeApp(firebaseConfig);
+export { fb };
