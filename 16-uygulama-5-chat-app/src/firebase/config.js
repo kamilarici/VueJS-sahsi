65813ffb5e6eaa7f4f8f4022b55dc1e9d4e2d19e
KamilArici <kamil@testinvite.com>
2023-09-25 23:00:39 +0300

    firebase chat app

---
+++ 16-uygulama-5-chat-app/src/firebase/config.js
@@ -0,0 +1,18 @@
+import flrebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAcr3SFAVlFL6QQS7VraqHMJOagxLE3KFs",
+  authDomain: "blog-vue3-f8644.firebaseapp.com",
+  projectId: "blog-vue3-f8644",
+  storageBucket: "blog-vue3-f8644.appspot.com",
+  messagingSenderId: "243990427326",
+  appId: "1:243990427326:web:6821aa4583111c66dfd0ef",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const projectFirestore = firebase.firestore();
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectFirestore, timestamp };
