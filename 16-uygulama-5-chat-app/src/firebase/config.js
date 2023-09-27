813b90a4e256252f776a526ae03b62c8392379ae
KamilArici <kamil@testinvite.com>
2023-09-27 23:13:35 +0300

    edit

--- 16-uygulama-5-chat-app/src/firebase/config.js
+++ 16-uygulama-5-chat-app/src/firebase/config.js
@@ -3,13 +3,21 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAcr3SFAVlFL6QQS7VraqHMJOagxLE3KFs",
-  authDomain: "blog-vue3-f8644.firebaseapp.com",
-  projectId: "blog-vue3-f8644",
-  storageBucket: "blog-vue3-f8644.appspot.com",
-  messagingSenderId: "243990427326",
-  appId: "1:243990427326:web:6821aa4583111c66dfd0ef",
+  apiKey: "AIzaSyCkizJMKbwdMT58IAx20d_8ZEXWP2kS_yg",
+  authDomain: "auth-btk.firebaseapp.com",
+  projectId: "auth-btk",
+  storageBucket: "auth-btk.appspot.com",
+  messagingSenderId: "869234126265",
+  appId: "1:869234126265:web:5b3b1c4def7129d383da68",
 };
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAcr3SFAVlFL6QQS7VraqHMJOagxLE3KFs",
+//   authDomain: "blog-vue3-f8644.firebaseapp.com",
+//   projectId: "blog-vue3-f8644",
+//   storageBucket: "blog-vue3-f8644.appspot.com",
+//   messagingSenderId: "243990427326",
+//   appId: "1:243990427326:web:6821aa4583111c66dfd0ef",
+// };
 
 firebase.initializeApp(firebaseConfig);
 
