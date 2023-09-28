ca67024a0d3be3ce0c6e88cf462592cef5e80caa
KamilArici <kamil@testinvite.com>
2023-09-28 22:20:18 +0300

    navbar

--- 16-uygulama-5-chat-app/src/composables/useLogin.js
+++ 16-uygulama-5-chat-app/src/composables/useLogin.js
@@ -8,7 +8,7 @@ const login = async (email, password) => {
   try {
     const res = await projectAuth.signInWithEmailAndPassword(email, password);
     error.value = null;
-    console.log(res);
+
     return res;
   } catch (err) {
     console.log(err.value);
