0d53abd6ef5d896fcc07c3ee9e30f76cba01a693
KamilArici <kamil@testinvite.com>
2023-09-28 23:45:49 +0300

    getuser-

--- 16-uygulama-5-chat-app/src/components/Navbar.vue
+++ 16-uygulama-5-chat-app/src/components/Navbar.vue
@@ -1,24 +1,27 @@
 <template>
-  <nav>
+  <nav v-if="user">
     <div>
-      <p>hi (displayName)</p>
-      <p class="email">currently logged in as...'email'</p>
+      <p>hi {{ user.displayName }}</p>
+      <p class="email">currently logged in as {{ user.email }}</p>
     </div>
     <button class="pointer" @click="handleClick">Logout</button>
   </nav>
 </template>
 <script>
 import useLogout from "@/composables/useLogout";
+import getUser from "../composables/getUser";
+
 export default {
   setup() {
     const { logout, error } = useLogout();
+    const { user } = getUser();
     const handleClick = async () => {
       await logout();
       if (!error.value) {
         console.log("user logged out");
       }
     };
-    return { handleClick };
+    return { handleClick, getUser, user };
   },
 };
 </script>
