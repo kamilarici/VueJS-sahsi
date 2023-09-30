2a4a27d3b1f427b493673729aa695334c7f22817
KamilArici <kamil@testinvite.com>
2023-09-30 23:13:30 +0300

    config.js

---
+++ 17-playlist-uygulamma-6/src/firebase/config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDoBI37wFg3TTholhaAULWn40IjSNtk2k4",
+  authDomain: "playlist-vue-btk-d6769.firebaseapp.com",
+  projectId: "playlist-vue-btk-d6769",
+  storageBucket: "playlist-vue-btk-d6769.appspot.com",
+  messagingSenderId: "144357594614",
+  appId: "1:144357594614:web:274b7741ac88cb88b723d7",
+};
