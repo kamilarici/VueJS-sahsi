e6a5fbddc5f4bf23cfc4603370e357e6962a9f5d
KamilArici <kamil@testinvite.com>
2023-10-01 10:51:31 +0300

    login

--- 17-playlist-uygulamma-6/src/router/index.js
+++ 17-playlist-uygulamma-6/src/router/index.js
@@ -7,6 +7,11 @@ const routes = [
     name: "home",
     component: Home,
   },
+  {
+    path: "/login",
+    name: "login",
+    component: () => import("../views/auth/Login.vue"),
+  },
 ];
 
 const router = createRouter({
---
+++ 17-playlist-uygulamma-6/src/views/auth/Login.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <form>
+      <h3>Login</h3>
+      <input type="email" placeholder="email" v-model="email" />
+      <input type="password" placeholder="Password" v-model="password" />
+      <button>Login</button>
+    </form>
+  </div>
+</template>
+<script>
+import useLogin from "@/composables/useLogin";
+import { ref } from "vue";
+export default {
+  setup() {
+    const { error, login } = useLogin();
+    const email = ref("");
+    const password = ref("");
+
+    const handleSubmit = async () => {
+      const res = await login(email.value, password.value);
+      if (!error.value) {
+        console.log("uder logged in");
+      }
+    };
+  },
+};
+</script>
+<style></style>
