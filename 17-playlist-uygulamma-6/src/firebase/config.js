26353ba50e5da043add6f81baeb679c144243e62
KamilArici <kamil@testinvite.com>
2023-10-02 23:24:20 +0300

    useStorage

--- 17-playlist-uygulamma-6/src/firebase/config.js
+++ 17-playlist-uygulamma-6/src/firebase/config.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
+import "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDoBI37wFg3TTholhaAULWn40IjSNtk2k4",
@@ -15,7 +16,8 @@ firebase.initializeApp(firebaseConfig);
 
 const projectFirestore = firebase.firestore();
 const projectAuth = firebase.auth();
+const projectStorage = firebase.storage();
 
 const timestamp = firebase.firestore.FieldValue.serverTimestamp;
 
-export { projectFirestore, projectAuth, timestamp };
+export { projectFirestore, projectAuth, timestamp, projectStorage };
