e4f24e6655db7c4b2666f633bc1e8f6832b7a867
KamilArici <kamil@testinvite.com>
2023-10-03 22:42:00 +0300

    list view

---
+++ 17-playlist-uygulamma-6/src/components/ListView.vue
@@ -0,0 +1,54 @@
+<template>
+  <div v-for="playlist in playlists" :key="palylist.id">
+    <div class="single">
+      <div class="thumbnail">
+        <img :src="palylist.coverUrl" />
+      </div>
+      <div class="info">
+        <h3>{{ palylist.title }}</h3>
+        <p>Created by {{ playlist.userName }}</p>
+      </div>
+      <div class="song-number">
+        <p>{{ palylist.songs.lenght }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ["playlist"],
+};
+</script>
+<style scoped>
+.single {
+  display: flex;
+  align-items: center;
+  padding: 20px;
+  border-radius: 10px;
+  background: white;
+  margin: 16px 0;
+  transition: all ease 0.2s;
+}
+.single:hover {
+  box-shadow: 1px 2px 3px rgba(50, 50, 50, 0.05);
+  transform: scale(1.02);
+  transition: all ease 0.2s;
+}
+.thumbnail {
+  max-width: 100px;
+  max-height: 100px;
+  overflow: hidden;
+  border-radius: 10px;
+}
+img {
+  max-width: 150%;
+  max-height: 150%;
+  display: block;
+}
+.info {
+  margin: 0 30px;
+}
+.song-number {
+  margin-left: auto;
+}
+</style>
--- 17-playlist-uygulamma-6/src/views/Home.vue
+++ 17-playlist-uygulamma-6/src/views/Home.vue
@@ -1,10 +1,22 @@
 <template>
-  <div class="home">home</div>
+  <div class="home">
+    <div v-if="error" class="error">Could not fetch the data.</div>
+    <div v-if="documents">
+      <div v-for="doc in documents" :key="doc.id">
+        {{ doc.title }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import getCollection from "@/composables/getCollection";
 export default {
   name: "HomeView",
-  components: {},
+  setup() {
+    const { error, documents } = getCollection("playlists");
+
+    return { error, documents };
+  },
 };
 </script>
