2330deed7bc1ab0d59bf9b72837535632e75bbce
KamilArici <kamil@testinvite.com>
2023-10-03 22:50:31 +0300

    playlist listeleme

--- 17-playlist-uygulamma-6/src/components/ListView.vue
+++ 17-playlist-uygulamma-6/src/components/ListView.vue
@@ -1,22 +1,22 @@
 <template>
-  <div v-for="playlist in playlists" :key="palylist.id">
+  <div v-for="playlist in playlists" :key="playlist.id">
     <div class="single">
       <div class="thumbnail">
-        <img :src="palylist.coverUrl" />
+        <img :src="playlist.coverUrl" />
       </div>
       <div class="info">
-        <h3>{{ palylist.title }}</h3>
+        <h3>{{ playlist.title }}</h3>
         <p>Created by {{ playlist.userName }}</p>
       </div>
       <div class="song-number">
-        <p>{{ palylist.songs.lenght }}</p>
+        <p>{{ playlist.songs.lenght }}</p>
       </div>
     </div>
   </div>
 </template>
 <script>
 export default {
-  props: ["playlist"],
+  props: ["playlists"],
 };
 </script>
 <style scoped>
--- 17-playlist-uygulamma-6/src/views/Home.vue
+++ 17-playlist-uygulamma-6/src/views/Home.vue
@@ -2,17 +2,17 @@
   <div class="home">
     <div v-if="error" class="error">Could not fetch the data.</div>
     <div v-if="documents">
-      <div v-for="doc in documents" :key="doc.id">
-        {{ doc.title }}
-      </div>
+      <ListView :playlists="documents" />
     </div>
   </div>
 </template>
 
 <script>
+import ListView from "@/components/ListView.vue";
 import getCollection from "@/composables/getCollection";
 export default {
   name: "HomeView",
+  components: { ListView },
   setup() {
     const { error, documents } = getCollection("playlists");
 
