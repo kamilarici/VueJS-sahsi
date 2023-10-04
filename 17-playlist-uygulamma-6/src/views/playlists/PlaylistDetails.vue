9952f34c709ef954d65e5020a7b5aa318263d5c0
KamilArici <kamil@testinvite.com>
2023-10-04 23:38:17 +0300

    dlete

--- 17-playlist-uygulamma-6/src/views/playlists/PlaylistDetails.vue
+++ 17-playlist-uygulamma-6/src/views/playlists/PlaylistDetails.vue
@@ -9,19 +9,70 @@
         <h2>{{ playlist.title }}</h2>
         <p class="username">Created by {{ playlist.userName }}</p>
         <p class="description">{{ playlist.description }}</p>
+        <button v-if="ownership">Delete</button>
+      </div>
+      <div class="song-list">
+        <p>song list</p>
       </div>
     </div>
   </div>
 </template>
 <script>
 import getDocument from "@/composables/getDocument";
+import getUser from "@/composables/getUser";
+import { computed } from "vue";
 
 export default {
   props: ["id"],
   setup(props) {
     const { error, document: playlist } = getDocument("playlists", props.id);
-    return { error, playlist };
+    const { user } = getUser();
+
+    const ownership = computed(() => {
+      return (
+        playlist.value && user.value && user.value.uid === playlist.value.userId
+      );
+    });
+
+    return { error, playlist, ownership };
   },
 };
 </script>
-<style></style>
+<style>
+.playlist-details {
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  gap: 80px;
+}
+.cover {
+  overflow: hidden;
+  border-radius: 20px;
+  position: relative;
+  padding: 160px;
+}
+.cover img {
+  display: block;
+  position: absolute;
+  top: 0;
+  left: 0;
+  min-width: 100%;
+  min-height: 100%;
+}
+.playlist-info {
+  text-align: center;
+}
+.playlist-info h2 {
+  text-transform: capitalize;
+  font-size: 28px;
+  margin-top: 20px;
+}
+.playlist-info p {
+  margin-bottom: 20px;
+}
+.username {
+  color: #999;
+}
+.description {
+  text-align: left;
+}
+</style>
