10b79283df36589512c1e9973de820f002afe90d
KamilArici <kamil@testinvite.com>
2023-10-04 22:31:52 +0300

    edit

--- 17-playlist-uygulamma-6/src/views/playlists/CreatePlayList.vue
+++ 17-playlist-uygulamma-6/src/views/playlists/CreatePlayList.vue
@@ -20,6 +20,7 @@ import useStorage from "@/composables/useStorage";
 import useCollection from "@/composables/useCollection";
 import getUser from "@/composables/getUser";
 import { timestamp } from "@/firebase/config";
+import { useRouter } from "vue-router";
 
 export default {
   setup() {
@@ -27,6 +28,8 @@ export default {
     const { error, addDoc } = useCollection("playlists");
 
     const { user } = getUser();
+    const router = useRouter();
+
     const isPending = ref(false);
 
     const title = ref("");
@@ -37,7 +40,7 @@ export default {
       if (file.value) {
         isPending.value = true;
         await uploadImage(file.value);
-        await addDoc({
+        const res = await addDoc({
           title: title.value,
           description: description.value,
           userId: user.value.uid,
@@ -49,7 +52,7 @@ export default {
         });
         isPending.value = false;
         if (!error.value) {
-          console.log("playlist added");
+          router.push({ name: "playlistdetails", params: { id: res.id } });
         }
       }
     };
