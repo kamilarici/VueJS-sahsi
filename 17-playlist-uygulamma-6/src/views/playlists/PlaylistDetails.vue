4f37fa3ed644203e414a87d1841013494e2e51c0
KamilArici <kamil@testinvite.com>
2023-10-04 22:51:11 +0300

    details edit

--- 17-playlist-uygulamma-6/src/views/playlists/PlaylistDetails.vue
+++ 17-playlist-uygulamma-6/src/views/playlists/PlaylistDetails.vue
@@ -1,9 +1,27 @@
 <template>
-  <div>palylist details {{ id }}</div>
+  <div>
+    <div v-if="error" class="error">{{ error }}</div>
+    <div v-if="playlist" class="playlist-details">
+      <div class="playlist-info">
+        <div class="cover">
+          <img :src="playlist.coverUrl" />
+        </div>
+        <h2>{{ playlist.title }}</h2>
+        <p class="username">Created by {{ playlist.userName }}</p>
+        <p class="description">{{ playlist.description }}</p>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
+import getDocument from "@/composables/getDocument";
+
 export default {
   props: ["id"],
+  setup(props) {
+    const { error, document: playlist } = getDocument("playlists", props.id);
+    return { error, playlist };
+  },
 };
 </script>
 <style></style>
