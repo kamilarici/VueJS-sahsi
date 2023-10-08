8fafd6de5d6b110214cbd6b33e60abfdd867298e
KamilArici <kamil@testinvite.com>
2023-10-08 22:00:50 +0300

    edit

--- 18-vuex/src/components/AddTodo.vue
+++ 18-vuex/src/components/AddTodo.vue
@@ -1,20 +1,32 @@
 <template>
   <form @submit.prevent="addTodo">
-    <input type="text" v-model="todo" />
+    <input type="text" placeholder="todo ekle" v-model="todo" />
     <button>Ekle</button>
   </form>
 </template>
 <script>
 import { ref } from "vue";
-import { useRouter } from "vue-router";
+import { useStore } from "vuex";
+
+// import { useRouter } from "vue-router";
 export default {
   setup() {
-    const router = useRouter();
-    const todo = ref(null);
+    const store = useStore();
+    const todo = ref("");
     const addTodo = () => {
-      router.push({ path: "/", query: { data: todo.value } });
-      todo.value = "";
+      const objectTodo = {
+        newTodo: todo.value,
+        done: true,
+      };
+      store.dispatch("addTodoAction", objectTodo);
     };
+    // const router = useRouter();
+    // const todo = ref(null);
+    // const addTodo = () => {
+    //   router.push({ path: "/", query: { data: todo.value } });
+    //   todo.value = "";
+    // };
+
     return { todo, addTodo };
   },
 };
--- 18-vuex/src/store/index.js
+++ 18-vuex/src/store/index.js
@@ -3,14 +3,37 @@ import { createStore } from "vuex";
 export default createStore({
   state: {
     todos: [
-      { id: 1, todo: "ders çaliş" },
-      { id: 2, todo: "film izle" },
-      { id: 3, todo: "kitap oku" },
-      { id: 4, todo: "spor yap" },
+      { id: 1, todo: "ders çaliş", done: false },
+      { id: 2, todo: "film izle", done: true },
+      { id: 3, todo: "kitap oku", done: false },
+      { id: 4, todo: "spor yap", done: true },
     ],
   },
-  getters: {},
-  mutations: {},
-  actions: {},
+  getters: {
+    todoTotalCount(state) {
+      return state.todos.length;
+    },
+    todoDoneCount(state) {
+      return state.todos.filter((x) => x.done == true).length;
+    },
+    todoNotCount(state) {
+      return state.todos.filter((x) => x.done == false);
+    },
+  },
+  mutations: {
+    addTodo(state, { newTodo, done }) {
+      let todo = {
+        id: Math.floor(Math.random() * 1000000),
+        todo: newTodo,
+        done: done,
+      };
+      state.todos.push(todo);
+    },
+  },
+  actions: {
+    addTodoAction(contex, todoObject) {
+      contex.commit("addTodo", todoObject);
+    },
+  },
   modules: {},
 });
