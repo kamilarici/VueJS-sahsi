2cb649d2979bc5c54077456be742c8c4033708b2
KamilArici <kamil@testinvite.com>
2023-10-08 19:34:34 +0300

    edit

--- 18-vuex/src/views/Home.vue
+++ 18-vuex/src/views/Home.vue
@@ -3,7 +3,17 @@
 </template>
 
 <script>
+import { ref } from "vue";
 export default {
   name: home,
+  components: {},
+  setup() {
+    const todos = ref([
+      { id: 1, todo: "ders çaliş" },
+      { id: 2, todo: "film izle" },
+      { id: 3, todo: "kitap oku" },
+    ]);
+    return { todos };
+  },
 };
 </script>
