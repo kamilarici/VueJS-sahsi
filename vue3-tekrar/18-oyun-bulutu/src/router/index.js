931b95dc43d8a7251b74c3f0580eef71dcdd4c1a
kamilarici <kamil@testinvite.com>
2023-10-12 22:58:28 +0300

    edit

--- vue3-tekrar/18-oyun-bulutu/src/router/index.js
+++ vue3-tekrar/18-oyun-bulutu/src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '../views/Home.vue'
+import Hakkimizda from '../views/Hakkimizda.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: Home
+  },
+  {
+    path: '/hakkimizda',
+    name: 'Hakkimizda',
+    component: Hakkimizda
   },
  
 ]
---
+++ vue3-tekrar/18-oyun-bulutu/src/views/Hakkimizda.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="about"> 
+    <h1>Hakkımızda</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod aperiam cupiditate quam impedit modi! Rerum beatae dolorem laborum est natus!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod aperiam cupiditate quam impedit modi! Rerum beatae dolorem laborum est natus!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod aperiam cupiditate quam impedit modi! Rerum beatae dolorem laborum est natus!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod aperiam cupiditate quam impedit modi! Rerum beatae dolorem laborum est natus!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod aperiam cupiditate quam impedit modi! Rerum beatae dolorem laborum est natus!</p>
+</div>
+</template>
+<script>
+export default{
+    name:'Hakkimizda'
+}
+</script>
+<style>
+.about{
+    max-width: 600px;
+    margin: 20px auto;
+}
+</style>
