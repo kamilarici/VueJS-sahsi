c25e122065bd538712e010c938b6ed82d1654bbe
kamilarici <kamil@testinvite.com>
2023-10-13 23:41:30 +0300

    edit

--- vue3-tekrar/18-oyun-bulutu/src/views/Home.vue
+++ vue3-tekrar/18-oyun-bulutu/src/views/Home.vue
@@ -1,34 +1,33 @@
 <template>
   <div class="home">
- <div>
-   <h1>AOS oyun bulutu</h1>
-   <p>Buluta oyun eklemek ve oyun görmek için giriş yapınız </p>
-   <button>Misafir Girişi</button>
- </div>  
+    <div>
+      <h1>AOS oyun bulutu</h1>
+      <p>Buluta oyun eklemek ve oyun görmek için giriş yapınız</p>
+      <button @click="handleGiris">Misafir Girişi</button>
+    </div>
   </div>
- </template>
- 
- <script>
- import girisYap from  '../composables/girisYap'
- import { useRouter } from 'vue-router';
+</template>
 
- 
- 
- export default {
-   name: 'Home',
-setup(){
-  const {login}=girisYap()
-  const useRouter=useRouter()
-  const handleGiris=async()=>{
-    await login()
-    router.push({name:'Ekle'})
-  }
+<script>
+import girisYap from "../composables/girisYap";
+import { useRouter } from "vue-router";
+
+export default {
+  name: "Home",
+  setup() {
+    const { login } = girisYap();
+    const useRouter = useRouter();
+    const handleGiris = async () => {
+      await login();
+      router.push({ name: "Ekle" });
+    };
+
+    return { handleGiris };
+  },
+};
+</script>
+<style scoped>
+button {
+  background-color: #fd7272;
 }
- }
- </script>
- <style scoped>
- button{
-   background-color:#FD7272  ;
- }
- </style>
- 
+</style>
