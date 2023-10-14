e10bedee573c76a10925ef247e4f09ba9884abbf
kamilarici <kamil@testinvite.com>
2023-10-15 01:20:19 +0300

    transition

--- vue3-tekrar/18-oyun-bulutu/src/views/Home.vue
+++ vue3-tekrar/18-oyun-bulutu/src/views/Home.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="home">
     <div>
+    <transition name="fade">
+      <div  v-if="showMerhaba" >
+        merhaba transition
+      </div>
+    </transition>
+    <button @click="showMerhaba=!showMerhaba">goster/gizle</button>
       <h1>AOS oyun bulutu</h1>
       <p>Buluta oyun eklemek ve oyun görmek için giriş yapınız</p>
       <button @click="handleGiris">Misafir Girişi</button>
@@ -9,12 +15,14 @@
 </template>
 
 <script>
+import { ref } from "vue";
 import girisYap from "../composables/girisYap";
 import { useRouter } from "vue-router";
 
 export default {
   name: "Home",
   setup() {
+    const showMerhaba=ref(true)
     const { login } = girisYap();
     const router = useRouter();
     const handleGiris = async () => {
@@ -22,12 +30,31 @@ export default {
       router.push({ name: "Ekle" });
     };
 
-    return { handleGiris };
+    return { handleGiris,showMerhaba };
   },
 };
 </script>
 <style scoped>
 button {
   background-color: #fd7272;
+}
+
+.fade-enter-from{
+  opacity: 0;
+}
+.fade-enter-to{
+  opacity: 1;
+}
+.fade-enter-active{
+  transition:all 2s ease;;
+}
+.fade-leave-from{
+  opacity: 1;
+}
+.fade-leave-to{
+  opacity: 0;
+}
+.fade-leave-active{
+  transition:all 2s ease;;
 }
 </style>
