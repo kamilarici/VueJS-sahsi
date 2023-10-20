c09b3e51cb7495b027c7d2e658c87cd3aeac94b3
kamilarici <kamil@testinvite.com>
2023-10-20 23:37:58 +0300

    edit

--- 19-fikir-bulutu/src/router/index.js
+++ 19-fikir-bulutu/src/router/index.js
@@ -11,7 +11,7 @@ const routes = [
     path: '/about',
     name: 'about',
    
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import( '../views/AboutView.vue')
   }
 ]
 
