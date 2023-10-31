9b53cb8b6a9814ab9852d267c1d59933b81e0389
kamilarici <kamil@testinvite.com>
2023-10-31 22:28:22 +0300

    config

---
+++ vue3-udemy/001-projeler/vue3-is-takip-app/src/firebase/config.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDrn_wYnupiGyk4ignIB_L4S6sfVxzm9gE",
+    authDomain: "aos-sosyal-app-dbc85.firebaseapp.com",
+    projectId: "aos-sosyal-app-dbc85",
+    storageBucket: "aos-sosyal-app-dbc85.appspot.com",
+    messagingSenderId: "621442397636",
+    appId: "1:621442397636:web:3da12516eb7b995d70a091"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+  const firestoreRef=firebase.firestore()
+  const authRef=firebase.auth()
