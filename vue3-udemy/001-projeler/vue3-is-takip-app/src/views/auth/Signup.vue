79157a3f9090d4202f72ff32927424029b906738
kamilarici <kamil@testinvite.com>
2023-11-02 22:25:11 +0300

    sign up

---
+++ vue3-udemy/001-projeler/vue3-is-takip-app/src/views/auth/Signup.vue
@@ -0,0 +1,33 @@
+<template>
+    <form @submit.prevent="handleSubmit" >
+  <h3>AOS iş takip üye olma  Sayfası</h3>
+  <input type="text" placeholder="Lütfen Kullanıcı Addınızı Giriniz" v-model="kullaniciAd">
+  <input type="email" placeholder="Lütfen Email Adresini Giriniz" v-model="email">
+  <input type="password" placeholder="Lütfen Parolanizi Giriniz" v-model="parola">
+  <button>Giriş</button>
+  <div v-if="hata" class="error">{{ hata }}</div>
+    </form>
+  </template>
+  
+  <script>
+  import useLogin from '@/composables/useLogin'
+  import {ref} from 'vue'
+  export default {
+  setup() {
+      const {hata,login}=useLogin();
+      const email=ref('')
+      const parola=ref('')
+      const handleSubmit=async()=>{
+          const res=await login(email.value, parola.value)
+          if(!hata.value){
+              console.log('kullanici giriş yapti')
+          }
+      }
+      return {email,parola,handleSubmit,hata}
+  }
+  }
+  </script>
+  
+  <style>
+  
+  </style>
