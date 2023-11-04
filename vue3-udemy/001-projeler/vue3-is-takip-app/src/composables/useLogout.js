8406a22521c8f470d5cd34aef6081f855cc98fb6
kamilarici <kamil@testinvite.com>
2023-11-05 00:27:13 +0300

    use logout

--- vue3-udemy/001-projeler/vue3-is-takip-app/src/composables/useLogout.js
+++ vue3-udemy/001-projeler/vue3-is-takip-app/src/composables/useLogout.js
@@ -1,3 +1,21 @@
-<template>
-    
-</template>
+import { ref } from "vue";
+import { authRef } from "@/firebase/config";
+
+const hata =ref(null)
+
+
+const logout=async()=>{
+    hata.value=null
+    try {
+        await authRef.signOut()
+    } catch (error) {
+        hata.value=err.message
+        
+    }
+}
+
+
+const useLogout=()=>{
+    return {hata,logout}
+}
+export default useLogout
