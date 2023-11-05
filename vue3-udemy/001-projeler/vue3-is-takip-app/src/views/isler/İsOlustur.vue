39a17e8b7d719e0373b78e3c7f68b1e351722f94
kamilarici <kamil@testinvite.com>
2023-11-05 11:10:02 +0300

    edit

--- vue3-udemy/001-projeler/vue3-is-takip-app/src/views/isler/İsOlustur.vue
+++ vue3-udemy/001-projeler/vue3-is-takip-app/src/views/isler/İsOlustur.vue
@@ -5,9 +5,9 @@
         <input type="text" placeholder="İş Başlik" v-model="baslik">
         <textarea  placeholder="İş Açiklama" v-model="aciklama"></textarea>
         <label for="">İş Tanitici Resim</label>
-        <input type="file">
+        <input type="file" @change="handleChange">
         <input type="datetime-local" v-model="basTarih">
-        <div class="error"></div>
+        <div class="error">{{ fileHata }}</div>
         <button>İş Oluştur</button>
 
     </form>
@@ -21,12 +21,33 @@ export default {
         const baslik=ref('')
         const aciklama=ref('')
         const basTarih=ref('')
+        const file=ref(null)
+        const fileHata=ref(null)
+
+      const  gecerliTipler=['image/png','image/jpeg']
+
 
         const handleSubmit=()=>{
             console.log(baslik.value,aciklama.value,basTarih.value)
         }
 
-        return {baslik,aciklama,basTarih,handleSubmit}
+        const handleChange=(e)=>{
+let secilen=e.target.files[0]
+// console.log(secilen)
+
+if(secilen && gecerliTipler.includes(secilen.type)){
+
+  file.value=secilen;
+  fileHata.value=null
+}else{
+  file.value=null
+  fileHata.value='lütfen png yada jpeg uzantılı dosyalar secin'
+}
+
+
+        }
+
+        return {baslik,aciklama,basTarih,handleSubmit,handleChange,fileHata}
     }
 
 }
