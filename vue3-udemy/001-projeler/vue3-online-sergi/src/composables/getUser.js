156ca31360b04d36efba62179980ccc5f15c824b
kamilarici <kamil@testinvite.com>
2023-12-05 22:33:09 +0300

    edit

---
+++ vue3-udemy/001-projeler/vue3-online-sergi/src/composables/getUser.js
@@ -0,0 +1,20 @@
+import {ref} from 'vue'
+import {authRef} from '../firebase/config'
+
+
+//refs
+const kullanici=ref(authRef.currentUser)
+
+
+//auth changes
+authRef.onAuthStateChanged(k=>{
+    kullanici.value=k
+})
+
+
+const getUser=()=>{
+    return {kullanici}
+}
+
+
+export default getUser
