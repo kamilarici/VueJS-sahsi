b34b3788004c52dc503022822fa98e91665bf73e
kamilarici <kamil@testinvite.com>
2023-12-05 22:46:22 +0300

    css navbar

--- vue3-udemy/001-projeler/vue3-online-sergi/src/main.js
+++ vue3-udemy/001-projeler/vue3-online-sergi/src/main.js
@@ -1,5 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import { authRef } from "./firebase/config";
 
-createApp(App).use(router).mount('#app')
+import "./assets/main.css";
+
+let app;
+
+authRef.onAuthStateChanged(() => {
+  if (!app) {
+    app = createApp(App).use(router).mount("#app");
+  }
+});
