9c5f565f46fc074dd0dfb090e8a95d19ea5819de
kamilarici <kamil@testinvite.com>
2024-02-10 14:30:26 +0300

    input -select binding

--- vue3-udemy/10-tekrar/src/App.vue
+++ vue3-udemy/10-tekrar/src/App.vue
@@ -1,13 +1,15 @@
 <template>
-
+<Form/>
 </template>
 
 <script>
-
+import Form from "./components/Form"
 
 export default {
   name: 'App',
-
+components:{
+  Form
+}
 }
 </script>
 
---
+++ vue3-udemy/10-tekrar/src/components/Form.vue
@@ -0,0 +1,28 @@
+<template>
+  <form >
+    <label > Email:</label>
+    <input v-model="email" type="email"/>
+<label >Yetki</label>
+<select v-model="yetki" >
+    <option  value="1">editor</option>
+    <option  value="2">admin</option>
+</select>
+{{ yetki }}
+  </form>
+</template>
+
+<script>
+
+export default {
+data() {
+    return {
+        email:"dfgd",
+        yetki:"yyy"
+    }
+},
+}
+</script>
+
+<style>
+
+</style>
