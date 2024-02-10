fea26b9bf96c090470dc99fd11af83e4951b7e66
kamilarici <kamil@testinvite.com>
2024-02-10 23:57:12 +0300

    todo start

---
+++ vue3-udemy/13-z-todotekrar/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+
+    <router-view/>
+  </div>
+ 
+</template>
+
+<style>
+body{
+  background-color: #2f3542;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+color: white;
+  max-width: 800px;
+  margin: 0 auto;
+}
+
+
+</style>
---
+++ vue3-udemy/13-z-todotekrar/src/views/HomeView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+<div v-if="yapilacaklar.length">
+  <div v-for="yap in yapilacaklar" :key="yap.id">
+ <p>{{ yap.baslik }}</p>
+</div>
+</div>
+
+<div v-else>
+  <p>yapilacaklar yükleniyor</p>
+
+
+</div>
+  </div>
+</template>
+
+<script>
+
+
+
+export default {
+  name: 'HomeView',
+  data() {
+    return {
+      yapilacaklar:[]
+    }
+  },
+  mounted() {
+    fetch('http://localhost:3000/yapilacaklar')
+    .then((res)=>res.json())
+    .then((data)=>this.yapilacaklar=data)
+    .catch((err)=>console.log(err))
+  },
+
+}
+</script>
