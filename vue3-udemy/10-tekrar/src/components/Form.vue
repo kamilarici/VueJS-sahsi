d484e7156d25626aaec553b91aeb151ba1426ff1
kamilarici <kamil@testinvite.com>
2024-02-10 15:01:56 +0300

    checkbox

--- vue3-udemy/10-tekrar/src/components/Form.vue
+++ vue3-udemy/10-tekrar/src/components/Form.vue
@@ -3,11 +3,36 @@
     <label > Email:</label>
     <input v-model="email" type="email"/>
 <label >Yetki</label>
+<hr>
 <select v-model="yetki" >
+    <option value="5">cccc</option>
     <option  value="1">editor</option>
     <option  value="2">admin</option>
+    <option  value="3">aaa</option>
+    <option  value="4">bbbb</option>
+    <option  value="6">ddd</option>
 </select>
-{{ yetki }}
+<hr>
+<input v-model="anlasma" type="checkbox"/>
+<label for="">Kabul ediyorum</label>
+<hr>
+<!-- <p>Anlaşma Kabulü: {{ anlasma ? 'Ediyorum' :'Etmiyorum'}}</p> -->
+<div>
+<input type="checkbox" value="yönetim" v-model="istekler"/>
+<label for="">yönetim</label>
+
+</div>
+<div>
+<input type="checkbox" value="erişim" v-model="istekler"/>
+<label for="">Erişim</label>
+
+</div>
+<div>
+<input type="checkbox" value="silme" v-model="istekler"/>
+<label for="">Silme</label>
+
+</div>
+<p>İstekler:{{ istekler }}</p>
   </form>
 </template>
 
@@ -17,7 +42,9 @@ export default {
 data() {
     return {
         email:"dfgd",
-        yetki:"yyy"
+        yetki:"5",
+        anlasma:true,
+        istekler:[]
     }
 },
 }
